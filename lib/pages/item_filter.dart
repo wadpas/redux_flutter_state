b12d77fa838ce204055598b4b1fcfbac67e39ba9
wadpas <wadpas@gmail.com>
2023-09-20 19:29:46 +0300

    created State class

--- lib/pages/item_filter.dart
+++ lib/pages/item_filter.dart
@@ -6,6 +6,39 @@ enum ItemFilter {
   shortText,
 }
 
+@immutable
+class State {
+  final Iterable<String> items;
+  final ItemFilter filter;
+
+  const State({
+    required this.items,
+    required this.filter,
+  });
+
+  Iterable<String> get filteredItems {
+    switch (filter) {
+      case ItemFilter.all:
+        return items;
+      case ItemFilter.longText:
+        return items.where((item) => items.length >= 10);
+      case ItemFilter.shortText:
+        return items.where((item) => items.length <= 3);
+    }
+  }
+}
+
+@immutable
+class ChangeFilterTypeAction extends Action {
+  final ItemFilter filter;
+  const ChangeFilterTypeAction({required this.filter});
+}
+
+@immutable
+abstract class Action {
+  const Action();
+}
+
 class ItemFilterPage extends StatelessWidget {
   const ItemFilterPage({super.key});
 
